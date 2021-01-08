9fb7c9621a8b8d485d3494dda990b8f4ce6725b6
Martin <martinald467@gmail.com>
2021-01-08 17:11:56 +0100

    rename

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar poly 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar laptop 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
