581a7d71f755405437be500ec51af21d5ccd5a2d
Martin <martinald467@gmail.com>
2021-03-21 19:44:33 +0100

    remove latest images of maven projects

--- bash/scripts/docker.sh
+++ bash/scripts/docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 dkrminone() {
-  dk rmi $(dk images -a | grep '<none>' | awk --field-separator=' ' '{ print $3 }')
+  sudo docker rmi $(dk images -a | grep '<none>' | awk --field-separator=' ' '{ print $3 }')
+}
+
+dkrmimavenlatest() {
+  local projs=$(find . -type f -name 'pom.xml' -printf '%h\n')
+
+  sudo docker rmi $(for i in $projs; do echo $(basename $i):latest; done)
 }
