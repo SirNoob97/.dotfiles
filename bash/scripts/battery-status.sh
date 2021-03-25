401645295983009bd0905348ff0de12926f73bf8
Martin <martinald467@gmail.com>
2021-03-25 20:12:57 +0100

    add battery script for polybar

---
+++ bash/scripts/battery-status.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function output {
+  if (($battery_level < 20)); then echo "  $1 $battery_level%"; fi
+  if (($battery_level < 50 && $battery_level >= 20)); then echo "  $1 $battery_level%"; fi
+  if (($battery_level < 80 && $battery_level >= 50)); then echo "  $1 $battery_level%"; fi
+  if (($battery_level < 99 && $battery_level >= 80)); then echo "  $1 $battery_level%"; fi
+}
+
+function check-status {
+  battery_level=$(acpi --battery | awk --field-separator=', ' '{ print $2 }' | tr --delete '%')
+  battery_status=$(acpi --battery | awk --field-separator=' ' '{ print $3 }' | tr --delete ',')
+  
+  case "$battery_status" in
+    "Charging") output "ﮣ";;
+    "Discharging") output;;
+    "Full") echo "  100%";;
+    "Unknow") echo " Unknown Status";;
+  esac
+}
+
+check-status
