b12da6bd8ee1ff25a75995f567bfbe960430c6ca
SirNoob97 <martinald467@gmail.com>
2021-08-23 23:32:32 +0200

    add minimalistic settings for nvim tree

--- nvim/.config/nvim/lua/config/nv_tree.lua
+++ nvim/.config/nvim/lua/config/nv_tree.lua
@@ -1,8 +1,24 @@
+vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
+vim.g.nvim_tree_gitignore = 1
+--vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_add_trailing = 1
+vim.g.nvim_tree_lsp_diagnostics = 1
+vim.g.nvim_tree_symlink_arrow = ' >> '
+
+vim.g.nvim_tree_special_files = { {'README.md', 1}, {'Makefile', 1}, {'MAKEFILE', 1} }
+vim.g.nvim_tree_show_icons = {
+  { 'git', 0,},
+  { 'folders', 0,},
+  { 'files', 0,},
+  { 'folder_arrows', 0,}
+}
+
 vim.api.nvim_set_keymap('', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 vim.g.nvim_tree_bindings = {
-      { key = "?", cb = tree_cb('toggle_help') }
+      { key = '?', cb = tree_cb('toggle_help') }
     }
 
 --set termguicolors -- this variable must be enabled for colors to be applied properly
