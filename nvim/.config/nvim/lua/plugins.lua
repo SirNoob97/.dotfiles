17dd81b74a66db698876d2b54c54bd1dce990cfe
SirNoob97 <martinald467@gmail.com>
2021-08-25 00:21:24 +0200

    add plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -3,9 +3,16 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(
   function()
     use 'wbthomason/packer.nvim'
+
+    use 'lifepillar/vim-gruvbox8'
+
     use 'hoob3rt/lualine.nvim'
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/nvim-compe'
+    use 'akinsho/bufferline.nvim'
     use 'kyazdani42/nvim-tree.lua'
+
+    use 'folke/lsp-colors.nvim'
+    use 'neovim/nvim-lspconfig'
+    use 'mhartington/formatter.nvim'
+    use 'nvim-lua/completion-nvim'
   end
 )
