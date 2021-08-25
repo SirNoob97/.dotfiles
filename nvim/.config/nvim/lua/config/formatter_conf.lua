ff84baf73684faa71de3f28e8079ce7625692b03
SirNoob97 <martinald467@gmail.com>
2021-08-25 21:28:08 +0200

    add luafmt and fix mapping

--- nvim/.config/nvim/lua/config/formatter_conf.lua
+++ nvim/.config/nvim/lua/config/formatter_conf.lua
@@ -2,28 +2,35 @@ local eslintD = {
   function()
     return {
       exe = "eslint_d",
-      args = { '--stdin', '--stdin-filename', '%filename', '--fix-to-stdout' },
+      args = {"--stdin", "--stdin-filename", "%filename", "--fix-to-stdout"},
       stdin = true
     }
   end
 }
 
-require('formatter').setup({
-  logging = false,
-  filetype = {
-    javascript =  eslintD,
-    typescript = eslintD
-    --lua = {
-    --    -- luafmt
-    --    function()
-    --      return {
-    --        exe = "luafmt",
-    --        args = {"--indent-count", 2, "--stdin"},
-    --        stdin = true
-    --      }
-    --    end
-    --}
+local luafmt = {
+  function()
+    return {
+      exe = "luafmt",
+      args = {"--indent-count", 2, "--line-width", 80, "--stdin"},
+      stdin = true
+    }
+  end
+}
+require("formatter").setup(
+  {
+    logging = false,
+    filetype = {
+      javascript = eslintD,
+      typescript = eslintD,
+      lua = luafmt
+    }
   }
-})
+)
 
-vim.api.nvim_set_keymap('n', '<leader>f', '<CMD>lua vim.lsp.buf.formatting()<CR>', { noremap=true, silent=true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>f",
+  "<CMD>Format<CR>",
+  {noremap = true, silent = true}
+)
