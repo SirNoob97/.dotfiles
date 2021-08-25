41746839642bcd68c84687cc5048e90235df1f53
SirNoob97 <martinald467@gmail.com>
2021-08-25 21:29:29 +0200

    inprove performance

--- nvim/.config/nvim/lua/config/nv_tree.lua
+++ nvim/.config/nvim/lua/config/nv_tree.lua
@@ -1,17 +1,22 @@
-vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
-vim.g.nvim_tree_gitignore = 1
-vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_disable_netrw = 0
+vim.g.nvim_tree_hijack_netrw = 0
+
+vim.g.nvim_tree_ignore = { '.git', '.mvn', 'node_modules', '.cache' }
 vim.g.nvim_tree_add_trailing = 1
-vim.g.nvim_tree_lsp_diagnostics = 1
 vim.g.nvim_tree_symlink_arrow = ' >> '
 
-vim.g.nvim_tree_special_files = { {'README.md', 1}, {'Makefile', 1}, {'MAKEFILE', 1} }
+-- unnecessary
+vim.g.nvim_tree_special_files = {}
+
+vim.g.nvim_tree_lsp_diagnostics = 1
+--vim.g.nvim_tree_git_hl = 1
+--vim.g.nvim_tree_icon_padding = ' '
 vim.g.nvim_tree_show_icons = {
-  { 'git', 0,},
-  { 'folders', 0,},
-  { 'files', 0,},
-  { 'folder_arrows', 0,}
+--   git = 1,
+   git = 0,
+   folders = 0,
+   files = 0,
+   folder_arrows = 0
 }
 
 vim.api.nvim_set_keymap('', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
@@ -19,9 +24,4 @@ vim.api.nvim_set_keymap('', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, si
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 vim.g.nvim_tree_bindings = {
       { key = '?', cb = tree_cb('toggle_help') }
-    }
-
---set termguicolors -- this variable must be enabled for colors to be applied properly
-
--- a list of groups can be found at `:help nvim_tree_highlight`
---highlight NvimTreeFolderIcon guibg=blue
+}
