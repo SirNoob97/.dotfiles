483e98d17674eb40de1a0936f635a6cc5d825e6e
SirNoob97 <martinald467@gmail.com>
2021-08-26 18:56:38 +0200

    remove lazy load

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,23 +1,18 @@
 vim.cmd [[packadd packer.nvim]]
-local ft = {
-  "lua",
-  "java",
-  "javascript",
-  "javascriptreact",
-  "typescript",
-  "typescriptreact"
-}
-return require("packer").startup(
-  function()
+
+local packer = require("packer")
+return packer.startup(
+  function(use)
     use "wbthomason/packer.nvim"
     use "lifepillar/vim-gruvbox8"
-    use  {"kyazdani42/nvim-tree.lua", opt = true, cmd = {"NvimTreeOpen", "NvimTreeToggle"}}
     use "hoob3rt/lualine.nvim"
-    use {"akinsho/bufferline.nvim", opt = true, event = {"BufEnter"}}
-    use "folke/lsp-colors.nvim"
+    use "akinsho/bufferline.nvim"
+    use "kyazdani42/nvim-tree.lua"
+
     use "neovim/nvim-lspconfig"
+    use "folke/lsp-colors.nvim"
     use "glepnir/lspsaga.nvim"
-    use {"mhartington/formatter.nvim", ft = ft}
-    use {"nvim-lua/completion-nvim", ft = ft}
+    use "mhartington/formatter.nvim"
+    use "nvim-lua/completion-nvim"
   end
 )
