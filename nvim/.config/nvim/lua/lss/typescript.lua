bf8f70fd07dbdcce207814f2e6c05d9df4adb346
SirNoob97 <martinald467@gmail.com>
2021-09-01 19:15:35 +0200

    add init options

--- nvim/.config/nvim/lua/lss/typescript.lua
+++ nvim/.config/nvim/lua/lss/typescript.lua
@@ -3,5 +3,11 @@ local lspConfig = require("lsp_config")
 require "lspconfig".tsserver.setup {
   on_attach = lspConfig.on_attach,
   filetypes = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
-  flags = lspConfig.flags
+  flags = lspConfig.flags,
+  init_options = {
+    preferences = {
+      quotePreference = "single",
+      importModuleSpecifierEnding = "index"
+    }
+  }
 }
