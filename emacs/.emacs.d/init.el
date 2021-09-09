d2c85cb38f1a0182d970853055be48a9880abed1
SirNoob97 <martinald467@gmail.com>
2021-09-09 22:27:17 +0200

    refactor

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,20 +1,10 @@
-(defun efs/display-startup-time ()
-  (message "Emacs loaded in %s with %d garbage collections."
-           (format "%.2f seconds"
-                   (float-time
-                     (time-subtract after-init-time before-init-time)))
-           gcs-done))
-
-(add-hook 'emacs-startup-hook #'efs/display-startup-time)
-
 (setq inhibit-startup-message t)
 
-(scroll-bar-mode -1)        ; Disable visible scrollbar
-(tool-bar-mode -1)          ; Disable the toolbar
-(tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
-
-(menu-bar-mode -1)            ; Disable the menu bar
+(scroll-bar-mode 0)        ; Disable visible scrollbar
+(tool-bar-mode 0)          ; Disable the toolbar
+(tooltip-mode 0)           ; Disable tooltips
+(set-fringe-mode 0)        ; Give some breathing room
+(menu-bar-mode 0)            ; Disable the menu bar
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
@@ -28,7 +18,7 @@
 
 (load-theme 'wombat t)
 
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "<escape>") 'keyboard-scape-quit)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
@@ -43,7 +33,7 @@
  ;; If there is more than one, they won't work right.
  '(lsp-java-java-path "/home/martin/.sdkman/candidates/java/current/bin/java")
  '(package-selected-packages
-   '(lsp-treemacs lsp-ivy which-key helpful ivy-rich doom-modeline counsel dap-mode lsp-java lsp-mode use-package cl-libify undo-tree s)))
+   '(company-lsp xclip lsp-ui company evil-collection flycheck lsp-treemacs lsp-ivy which-key helpful ivy-rich doom-modeline counsel dap-mode lsp-java lsp-mode use-package cl-libify undo-tree s)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -78,6 +68,11 @@
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
 
+;(use-package evil-collection
+;  :after evil
+;  :config
+;  (evil-collection-init))
+;
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
@@ -94,11 +89,6 @@
          ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
-
-;(use-package ivy-rich
-;  :after ivy
-;  :init
-;  (ivy-rich-mode 1))
 
 (use-package counsel
   :bind (("C-M-j" . 'counsel-switch-buffer)
@@ -121,43 +111,189 @@
   ([remap describe-key] . helpful-key))
 
 (use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
+  :hook (after-init . doom-modeline-mode))
 
 ; LSP
-(defun efs/lsp-mode-setup ()
-  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
-  (lsp-headerline-breadcrumb-mode))
+;(use-package flycheck
+;  :init
+;  (setq flycheck-check-syntax-automatically '(mode-enabled save))
+;  :custom
+;  (flycheck-highlighting-mode 'lines)
+;  (flycheck-highlighting-style 'leve-face))
+
+
+;----------------------
+(use-package yasnippet
+  :init
+  (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+  :config
+  (autoload 'yas/expand "yasnippet" t)
+  (autoload 'yas/load-directory "yasnippet" t)
+  (mapc 'yas/load-directory yas/root-directory)
+  (yas-global-mode 1))
+
+(defun default-java-code-style-hook()
+  (setq c-basic-offset 2
+        c-label-offset 0
+        tab-width 2
+        indent-tabs-mode nil
+        require-final-newline nil))
+(add-hook 'java-mode-hook 'default-java-code-style-hook)
+
+(use-package flycheck
+  :init
+  (add-to-list 'display-buffer-alist
+               `(,(rx bos "*Flycheck errors*" eos)
+                 (display-buffer-reuse-window display-buffer-in-side-window)
+                 (side . bottom)
+                 (reusable-frames . visible)
+                 (window-height . 0.15))))
+
+(defun custom-java-mode-hook ()
+  (auto-fill-mode)
+  (flycheck-mode)
+  (subword-mode)
+  (yas-minor-mode)
+  (set-fringe-style '(8 . 0))
+
+  ;; Fix indentation for anonymous classes
+  (c-set-offset 'substatement-open 0)
+  (if (assoc 'inexpr-class c-offsets-alist)
+      (c-set-offset 'inexpr-class 0))
+
+  ;; Indent arguments on the next line as indented body.
+  (c-set-offset 'arglist-intro '++))
+(add-hook 'java-mode-hook 'custom-java-mode-hook)
 
 (use-package lsp-mode
-  :commands (lsp lsp-deferred)
-  :hook (lsp-mode . efs/lsp-mode-setup)
-  :init (setq lsp-keymap-prefix "C-c l")
-  :config (lsp-enable-which-key-integration))
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  (setq lsp-headerline-breadcrumb-segments '(file symbols))
+  :bind
+  (:map lsp-mode-map
+        (("M-RET" . lsp-execute-code-action)))
+  :config
+  (setq lsp-inhibit-message t
+	lsp-completion-enable-additional-text-edit nil
+        lsp-eldoc-render-all nil
+        lsp-enable-file-watchers nil
+        lsp-enable-symbol-highlighting nil
+        lsp-headerline-breadcrumb-enable nil
+        lsp-highlight-symbol-at-point nil
+        lsp-modeline-code-actions-enable nil
+        lsp-modeline-diagnostics-enable nil)
+
+  ;; Performance tweaks, see
+  ;; https://github.com/emacs-lsp/lsp-mode#performance
+  (setq gc-cons-threshold 100000000)
+  (setq read-process-output-max (* 1024 1024)) ;; 1mb
+  (setq lsp-idle-delay 0.500))
 
 (use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode)
-  :custom (lsp-ui-doc-position 'bottom))
+  :config
+  (setq lsp-prefer-flymake nil
+        lsp-ui-doc-delay 5.0
+        lsp-ui-sideline-enable nil
+        lsp-ui-sideline-show-symbol nil))
 
-(use-package lsp-treemacs
-  :after lsp)
+(use-package lsp-java
+  :init
+  (setq lsp-java-vmargs
+        (list
+         "-noverify"
+         "-Xmx3G"
+         "-XX:+UseG1GC"
+         "-XX:+UseStringDeduplication"
+         "-javaagent:/home/martin/.m2/repository/org/projectlombok/lombok/1.18.20/lombok-1.18.20.jar"
+         )
 
-(use-package lsp-ivy
-  :after lsp)
+        ;; Don't organise imports on save
+        lsp-java-save-action-organize-imports nil
 
-(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
-(use-package company
-  :after lsp-mode
-  :hook (lsp-mode . company-mode)
-  :config (add-hook 'after-init-hook 'global-company-mode))
+        ;; Fetch less results from the Eclipse server
+        lsp-java-completion-max-results 20
 
-(with-eval-after-load 'company
- (define-key company-active-map (kbd "C-SPC") 'company-complete)
- (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
- (define-key company-active-map (kbd "<backtab>") (lambda () (interactive) (company-complete-common-or-cycle -1))))
+        lsp-java-java-path "/home/martin/.sdkman/candidates/java/current/bin/java"
+        )
+
+  :config
+  (add-hook 'java-mode-hook #'lsp))
 
 (use-package dap-mode
   :after lsp-mode
-  :config (dap-auto-configure-mode))
-(use-package dap-java :ensure nil)
+  :config
+  (dap-mode t)
+  (dap-ui-mode t)
+  (dap-tooltip-mode 1)
+  (tooltip-mode 1)
+  (dap-register-debug-template
+   "localhost:5005"
+   (list :type "java"
+         :request "attach"
+         :hostName "localhost"
+         :port 5005))
+  ;(dap-register-debug-template
+  ; "lxd"
+  ; (list :type "java"
+  ;       :request "attach"
+  ;       :hostName "x.x.x.x"
+  ;       :port 5005))
+  )
+
+(use-package dap-java
+  :ensure nil
+  :after (lsp-java)
+
+  ;; The :bind here makes use-package fail to lead the dap-java block!
+  ;; :bind
+  ;; (("C-c R" . dap-java-run-test-class)
+  ;;  ("C-c d" . dap-java-debug-test-method)
+  ;;  ("C-c r" . dap-java-run-test-method)
+  ;;  )
+
+  :config
+  (global-set-key (kbd "<f7>") 'dap-step-in)
+  (global-set-key (kbd "<f8>") 'dap-next)
+  (global-set-key (kbd "<f9>") 'dap-continue)
+  )
+
+(use-package treemacs
+  :init
+  (add-hook 'treemacs-mode-hook
+            (lambda () (treemacs-resize-icons 15))))
+
+
+;----------------------
+
+(use-package xclip
+  :config
+  (xclip-mode 1))
+
+
+;(use-package lsp-mode
+;  :hook
+;  (lsp-mode . lsp-enable-which-key-integration)
+;  :init
+;  (setq lsp-keymap-prefix "C-c l")
+;  (setq lsp-headerline-breadcrumb-segments '(file symbols))
+;  :config (setq lsp-completion-enable-additional-text-edit nil))
+
+(use-package company
+  :bind (:map company-active-map
+              ("TAB" . company-complete-common-or-cycle)
+	      ("<backtab>" . company-select-previous))
+  :custom
+  (company-idle-delay 0.0)
+  :config
+  (add-hook 'after-init-hook 'global-company-mode)
+  (company-mode))
+;
+;(use-package lsp-treemacs :after lsp) 
+;(use-package lsp-ivy :after lsp) 
+;(use-package lsp-ui
+;  :hook (lsp-mode . lsp-ui-mode)
+;  :custom (lsp-ui-doc-position 'bottom))
+
+;(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
+;(use-package dap-mode :after lsp-mode :config (dap-auto-configure-mode))
+;(use-package dap-java :ensure nil)
