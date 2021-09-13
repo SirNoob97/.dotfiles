fe9f77316d571c05246a1c893964106d17bbdc7e
SirNoob97 <martinald467@gmail.com>
2021-09-13 21:06:23 +0200

    add auto start script and audio module

---
+++ qtile/.config/qtile/autostart.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Sutostart script for Qtile
+
+# Set functions
+
+# Functions
+
+cmd_exist() { unalias "$1" >/dev/null 2>&1 ; command -v "$1" >/dev/null 2>&1 ;}
+__kill() { kill -9 "$(pidof "$1")" >/dev/null 2>&1 ; }
+__start() { sleep 1 && "$@" >/dev/null 2>&1 & }
+__running() { pidof "$1" >/dev/null 2>&1 ;}
+
+# Authentication dialog
+
+#if [ -f /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 ]; then
+#    __kill polkit-gnome-authentication-agent-1
+#    __start /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+#fi
+
+# Wallpaper manager
+
+if cmd_exist feh ; then
+    __kill feh
+    __start feh --randomize --bg-fill --no-fehbg /home/martin/Pictures/Wallpaper &
+
+fi
+
+# Notification daemon
+
+if cmd_exist dunst ; then
+    __kill dunst
+    __start dunst
+fi
+
+# Network manager
+
+#if cmd_exist nm-applet ; then
+    #__kill nm-applet
+    #__start nm-applet
+#fi
