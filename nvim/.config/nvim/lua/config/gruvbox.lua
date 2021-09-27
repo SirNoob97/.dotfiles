90c669319971d587151c9d5cc8be70597dc3d5fb
SirNoob97 <martinald467@gmail.com>
2021-09-27 21:55:54 +0200

    remove, to recover go to nvim branch

--- nvim/.config/nvim/lua/config/gruvbox.lua
+++
@@ -1,5 +0,0 @@
-vim.o.termguicolors = true
-vim.g.gruvbox_filetype_hi_groups = 0
-vim.g.gruvbox_italicize_strings = 1
-vim.g.gruvbox_plugin_hi_groups = 0
-vim.g.gruvbox_transp_bg = 0
--- nvim/.config/nvim/lua/disable_ntrw.lua
+++
@@ -1,4 +0,0 @@
-vim.api.nvim_set_var('loaded_netrw', 1)
-vim.api.nvim_set_var('loaded_netrwFileHandlers', 1)
-vim.api.nvim_set_var('loaded_netrwSettings', 1)
-vim.api.nvim_set_var('loaded_netrwPlugin', 1)
