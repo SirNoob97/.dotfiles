06348973a135df1afa36775b651991d9edcf5cbd
SirNoob97 <martinald467@gmail.com>
2022-02-10 15:59:04 +0100

    add explanation about the brightness file and conditionals

--- qtile/.config/qtile/modules/brightness.sh
+++ qtile/.config/qtile/modules/brightness.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# the 'brightness' file belongs to root, so keep that in mind
+
 basedir="/sys/class/backlight"
 handler="$basedir/$(ls $basedir)"
 
-old_brightness=$(cat "/$handler/brightness")
+old_brightness=$(cat "$handler/brightness")
 max_brightness=$(cat "$handler/max_brightness")
 
 # get current brightness %
@@ -11,6 +13,9 @@ old_brightness_p=$(( 100 * $old_brightness / $max_brightness ))
 
 # calculate new brightness %
 new_brightness_p=$(($old_brightness_p $1))
+
+[ 100 -lt $new_brightness_p ] && new_brightness_p=100
+[ 0 -ge $new_brightness_p ] && new_brightness_p=10
 
 # calculate new brightness value
 new_brightness=$(( $max_brightness * $new_brightness_p / 100 ))
