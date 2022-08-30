cef53528c0153cef3f52455485019f2a9f496f14
SirNoob97 <martinald467@gmail.com>
2022-08-30 17:12:16 +0200

    add network module to check wich interface is active --pending

---
+++ qtile/.config/qtile/modules/c-version/network.c
@@ -0,0 +1,61 @@
+/* Inspired by
+ * https://gist.github.com/edufelipe/6108057
+*/
+
+#include <ifaddrs.h>
+#include <linux/wireless.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int check_wireless(const char *ifname, char *protocol) {
+  int sock = -1;
+  struct iwreq pwrq;
+  memset(&pwrq, 0, sizeof(pwrq));
+  strncpy(pwrq.ifr_name, ifname, IFNAMSIZ);
+
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    perror("socket");
+    return 0;
+  }
+
+  if (ioctl(sock, SIOCGIWNAME, &pwrq) != -1) {
+    if (protocol)
+      strncpy(protocol, pwrq.u.name, IFNAMSIZ);
+    close(sock);
+    return 1;
+  }
+
+  close(sock);
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  struct ifaddrs *ifaddr, *ifa;
+
+  if (getifaddrs(&ifaddr) == -1) {
+    perror("getifaddrs");
+    return -1;
+  }
+
+  /* Walk through linked list, maintaining head pointer so we
+     can free list later */
+  for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+    char protocol[IFNAMSIZ] = {0};
+
+    if (ifa->ifa_addr == NULL || ifa->ifa_addr->sa_family != AF_PACKET)
+      continue;
+
+    if (check_wireless(ifa->ifa_name, protocol)) {
+      printf("interface %s is wireless: %s\n", ifa->ifa_name, protocol);
+    } else {
+      printf("interface %s is not wireless\n", ifa->ifa_name);
+    }
+  }
+
+  freeifaddrs(ifaddr);
+  return 0;
+}
